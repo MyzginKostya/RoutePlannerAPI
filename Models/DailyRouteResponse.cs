cdfb4a665571c3a9fbc1c928cd828f1fc47e5fd3
Kostya <myzginkosta@gmail.com>
2025-09-19 16:03:22 +0700

    Добавление комментариев в код. Рефакторинг и новый параметр - максимальное количество визитов в день.

--- Models/DailyRouteResponse.cs
+++ Models/DailyRouteResponse.cs
@@ -1,5 +1,6 @@
 ﻿namespace RoutePlannerAPI.Models
 {
+    // класс также как и DailyRoute служит для формирования результатов, только этот возвращает результат и не используется в коде для внесения изменений в процессе работы алгоритма
     public class DailyRouteResponse
     {
         public int DayNumber { get; set; }
--- Models/RoutePlanRequest.cs
+++ Models/RoutePlanRequest.cs
@@ -1,11 +1,15 @@
 ﻿namespace RoutePlannerAPI.Models
 {
+    /// <summary>
+    /// модель данных для расчета маршрута
+    /// </summary>
     public class RoutePlanRequest
     {
-        public int TotalDays { get; set; }
-        public double WorkingHoursPerDay { get; set; }
-        public bool UseFixedStartPoint { get; set; }
-        public long FixedStartPointId { get; set; }
-        public List<RouteSegmentInput> Segments { get; set; }
+        public int TotalDays { get; set; } // общее количество дней на которое строим маршрут
+        public double WorkingHoursPerDay { get; set; } // сколько часов в день может работать сотрудник
+        public bool UseFixedStartPoint { get; set; } // задана ли стартовая точка принудительно или нет
+        public long FixedStartPointId { get; set; } // идентификатор заданной стартовой точки
+        public int MaxCountVisists { get; set; } // максимальное количество визитов в день
+        public List<RouteSegmentInput> Segments { get; set; } // список точек
     }
 }
